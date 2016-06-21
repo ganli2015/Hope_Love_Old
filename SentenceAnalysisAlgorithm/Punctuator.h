f6a172ef876faaa06257eceb49cbdf67bc6f0bb0
ganli2015 <ganli2012@gmail.com>
2016-06-21 15:53:16 +0800

    Update Punctuator.h

--- SentenceAnalysisAlgorithm/Punctuator.h
+++ SentenceAnalysisAlgorithm/Punctuator.h
@@ -6,6 +6,7 @@ namespace DataCollection
 	class Sentence;
 }
 
+///Algorithm of punctuating a sentence.
 class _SENTENCEANALYSISALGORITHMINOUT Punctuator
 {
 	shared_ptr<DataCollection::Sentence> _unpunctuated;
@@ -13,6 +14,8 @@ public:
 	Punctuator(std::string longSen);
 	~Punctuator(void);
 	
+	///Punctuate the input sentence and the result will store in <punctuated> as several sub sentences.
+	///The input sentence will be separated according to end-of-sentence punctures. 
 	bool Punctuate(shared_ptr<DataCollection::Sentence>& punctuated);
 
 	//std::vector<shared_ptr<DataCollection::Sentence>> GetPunctuated() const {return _punctuated;}
