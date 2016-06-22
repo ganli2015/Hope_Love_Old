cff580ca1dd946d528c7c33a7d066caa4592a1d7
ganli2015 <ganli2012@gmail.com>
2016-06-22 10:50:53 +0800

    Update WordSegmentator.h

--- SentenceAnalysisAlgorithm/WordSegmentator.h
+++ SentenceAnalysisAlgorithm/WordSegmentator.h
@@ -14,27 +14,31 @@ class _SENTENCEANALYSISALGORITHMINOUT WordSegmentator
 {
     shared_ptr<DataCollection::Sentence> _unsegmented;
 
-	//分词后的句子，key储存子句子，value是该子句子对应的多个分词句子.
+	///Sentences after segmented.Key is strings of sentences and value is sentences.
 	multimap<string,shared_ptr<DataCollection::SegmentedSentence>> _segmented;
 public:
-	//sentence的标点符号必须是已经是puncture类
+	///<sentence> must be punctuated before.
 	WordSegmentator(shared_ptr<DataCollection::Sentence> sentence);
 	~WordSegmentator(void);
 
-
+	///Segment the input sentence into individual words.
+	///Those words have no definite POS.Their POS have not been deduced.
 	bool Segment();
 
 	vector<shared_ptr<DataCollection::SegmentedSentence>> GetAllSegementedSentence() const;
 
 private:
-
+	
 	struct CharacterProperty
 	{
 		DataCollection::Character character;
+		///All words in Cerebrum whose first character is <character>.
 		vector<DataCollection::Word> candidate;
+		///the index of <character> in a sentence.
 		int index;
 	};
 
+	///Segment each subsentence.
 	void SegmentSubsentence(const string subsentence);
 
 	std::vector<DataCollection::Character> GetRawSentence(shared_ptr<DataCollection::Sentence> sentence) const ;
